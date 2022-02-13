b44347deefa6ad87866d73864309e8222eb15ff6
sahar <sahar.mohsni@gomycode.co>
2022-02-13 22:04:34 +0100

    add padding

--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -50,17 +50,11 @@ const LoginForm = () => {
 						onChange={() => clearErrors()}
 					/>
 				</Form.Item>
-				<Form.Item>
-					<a className="login-form-forgot" href="">
-						Forgot password
-					</a>
-				</Form.Item>
 
 				<Form.Item>
 					<Button type="primary" htmlType="submit" className="login-form-button">
 						Log in <RightOutlined />
 					</Button>
-					Or <a href="">register now!</a>
 				</Form.Item>
 			</Form>
 		</div>
